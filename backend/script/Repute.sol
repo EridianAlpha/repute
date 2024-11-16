28728981552843a536b76263879d44abc4cebf72
Caronfire <caron.schaller@gmail.com>
2024-11-16 17:31:08 +0700

    Add commit and reveal of answers

--- backend/script/Repute.sol
+++ backend/script/Repute.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract Repute is Ownable {
+    using Math for uint256;
 
     constructor() Ownable(msg.sender) {}
 
@@ -14,9 +16,10 @@ contract Repute is Ownable {
 
     uint256 public oracleCount;
     uint256 public projectCount;
-    uint256 public voteCount;
+    uint256 public totalVoteCount;
 
     // Counter for vote rounds
+    uint256 public nextOracleId = 1;
     uint256 public nextVoteId = 1;
     uint256 public nextProjectId = 1;
 
@@ -31,6 +34,7 @@ contract Repute is Ownable {
     }
 
     struct Oracle {
+        uint256 id;
         uint256 volumeWon; // reputation
         uint256 volumeLost;
         uint256 volumePending;
@@ -44,20 +48,28 @@ contract Repute is Ownable {
         uint256 timestampFinalize; // Answer is finalized
         uint256 timestampEnd; // Reputations and payments are settled
 
-        mapping(address => bytes32) oracleCommitHash;
-        mapping(address => uint256) oracleRevealedAnswer;
+        mapping(address => bytes) oracleCommitHashes;
+        mapping(address => uint256) oracleRevealedAnswers;
+
+        address[] oraclesCommitted;
+        address[] oraclesRevealed;
+
+        uint256 oracleCommits;
+        uint256 oracleReveals;
 
         uint256 answer;
     }
 
     // Register as new oracle
     function registerOracle() external {
-        require(!blacklistedOracles[msg.sender], "Oracle already registered");
+        require(oracleMap[msg.sender].id == 0, "Oracle already registered");
         require(blacklistedOracles[msg.sender] != true, "Oracle is blacklisted");
 
-        oracleMap[msg.sender] = Oracle(0, 0, 0);
-
+        Oracle storage newOracle = oracleMap[msg.sender];
+        newOracle.id = nextOracleId;
         blacklistedOracles[msg.sender] = false;
+
+        nextOracleId++;
     }
 
     function updateOracleBlacklist(address oracle, bool blacklistOracle) onlyOwner external {
@@ -71,13 +83,6 @@ contract Repute is Ownable {
         newProject.owner = msg.sender;
 
         nextProjectId++;
-    }
-
-    // Update existing project
-    function updateProject(uint256 projectId) external {
-        require(projectMap[projectId].owner == msg.sender, "You are not the project owner.");
-
-        // TODO
     }
 
     // Register a new oracle
@@ -94,7 +99,117 @@ contract Repute is Ownable {
         newVote.timestampEnd = timestampEnd; // Reputations and payments are settled
 
         project.voteCount++;
+        totalVoteCount++;
 
         nextVoteId++;
+    }
+
+    function submitVote(uint256 projectId, uint256 voteId, bytes calldata hashedAnswer) external {
+        require(oracleMap[msg.sender].id != 0, "Oracle is not registered");
+        require(blacklistedOracles[msg.sender] != true, "Oracle is blacklisted");
+
+        Vote storage vote = projectMap[projectId].voteMap[voteId];
+
+        if (vote.oracleCommitHashes[msg.sender].length == 0) {
+            vote.oraclesCommitted.push(msg.sender);
+            vote.oracleCommits++;
+        }
+
+        vote.oracleCommitHashes[msg.sender] = hashedAnswer;
+    }
+
+    function revealVote(
+        uint256 projectId, 
+        uint256 voteId, 
+        uint256 timestamp, 
+        uint256 revealedAnswer
+    ) external {
+        // @TODO add timestamp requirement
+        
+        require(oracleMap[msg.sender].id != 0, "Oracle is not registered");
+        require(blacklistedOracles[msg.sender] != true, "Oracle is blacklisted");        
+
+        Vote storage vote = projectMap[projectId].voteMap[voteId];
+        
+        bool verification = ecverifyData(msg.sender, projectId, voteId, timestamp, revealedAnswer, vote.oracleCommitHashes[msg.sender]);
+        require(verification, "Data was improperly signed.");
+
+        if (vote.oracleRevealedAnswers[msg.sender] == 0) {
+            vote.oraclesRevealed.push(msg.sender);
+            vote.oracleReveals++;
+        }
+
+        vote.oracleRevealedAnswers[msg.sender] = revealedAnswer;
+    }
+
+    function computeAnswer(uint256 projectId, uint256 voteId) external {
+        // @TODO add timestamp requirement
+        Vote storage vote = projectMap[projectId].voteMap[voteId];
+
+        uint256 total = 0;
+        uint256 length = vote.oraclesCommitted.length;
+
+        require(length > 0, "No responses to average");
+
+        for (uint256 i = 0; i < length; i++) {
+            total += vote.oracleRevealedAnswers[vote.oraclesCommitted[i]];
+        }
+
+        vote.answer = total / length; 
+    }
+
+    function getAnswer(uint256 projectId, uint256 voteId) public view returns (uint256 answer) {
+        return projectMap[projectId].voteMap[voteId].answer;
+    }
+
+
+
+
+
+    // Verify data with signature
+    function ecverifyData(
+        address oracle,
+        uint256 projectId, 
+        uint256 voteId, 
+        uint256 timestamp,  
+        uint256 revealedAnswer,
+        bytes memory hashedAnswer        
+    ) public pure returns (bool) {
+
+        bytes32 hash = keccak256(abi.encodePacked(projectId, voteId, timestamp, revealedAnswer));
+        address recovered = ecrecovery(hash, hashedAnswer);
+    
+        return oracle == recovered;
+    }
+
+    // Recover an address from a message hash and a signature
+    function ecrecovery(
+        bytes32 hash,
+        bytes memory signature
+    ) private pure returns (address) {
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        if (signature.length != 65) {
+            return address(0);
+        }
+
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        if (v != 27 && v != 28) {
+            return (address(0));
+        } else {
+            return ecrecover(hash, v, r, s);
+        }
     }
 }
